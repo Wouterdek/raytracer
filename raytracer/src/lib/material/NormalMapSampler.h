8418cef65e51e1fd8552e235a9d94a9f509f4255
Wouter <wouterdekrsmckr@gmail.com>
2019-04-17 13:09:21 +0200

    Added normal map sampler

---
+++ raytracer/src/lib/material/NormalMapSampler.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <math/RayHitInfo.h>
+#include "math/Vector3.h"
+#include "Texture.h"
+
+inline Vector3 sample_normal_map(const RayHitInfo& hit, const Texture& normalMap)
+{
+    float x = abs(fmod(hit.texCoord.x(), 1.0f));
+    float y = abs(fmod(hit.texCoord.y(), 1.0f));
+    auto normalColor = normalMap.get(x * normalMap.getWidth(), y * normalMap.getHeight());
+    return Vector3(normalColor.getRed(), normalColor.getGreen(), normalColor.getBlue());
+}
