83d32e21ce1a7cfecd9fd3437c034318571b292f
Wouter <wouterdekrsmckr@gmail.com>
2019-04-17 13:10:08 +0200

    Refactored Texture to allow loading RGBA data

--- raytracer/src/lib/material/Texture.cpp
+++ raytracer/src/lib/material/Texture.cpp
@@ -2,36 +2,16 @@
 #include <vector>
 #include "io/lodepng.h"
 
-Texture::Texture()
-{
-}
+Texture::Texture(std::vector<unsigned char> image, unsigned width, unsigned height)
+    : image(std::move(image)), width(width), height(height)
+{}
 
-RGB Texture::get(unsigned int x, unsigned int y) const
+Texture Texture::loadPNG(std::string path)
 {
-	auto offset = ((y * this->width) + x) * 4; //Left to right, 4 bytes per pixel
-	auto r = this->image[offset];
-	auto g = this->image[offset+1];
-	auto b = this->image[offset+2];
-	auto a = this->image[offset+3];
-	return RGB((float)r/255.0f, (float)g / 255.0f, (float)b / 255.0f);
-}
-
-unsigned Texture::getWidth() const
-{
-	return width;
-}
-
-unsigned Texture::getHeight() const
-{
-	return height;
-}
-
-Texture Texture::load(std::string path)
-{
-	Texture tex;
-
 	//decode
-	unsigned error = lodepng::decode(tex.image, tex.width, tex.height, path);
+    std::vector<unsigned char> imageData;
+    unsigned width, height;
+	unsigned error = lodepng::decode(imageData, width, height, path);
 	
-	return tex;
+	return Texture(std::move(imageData), width, height);
 }
--- raytracer/src/lib/material/Texture.h
+++ raytracer/src/lib/material/Texture.h
@@ -7,12 +7,35 @@ class Texture
 private:
 	std::vector<unsigned char> image; //the raw pixels in RGBA order
 	unsigned width, height;
-	Texture();
 
 public:
-	static Texture load(std::string path);
+    Texture(std::vector<unsigned char> image, unsigned width, unsigned height);
+	static Texture loadPNG(std::string path);
 
-	RGB get(unsigned int x, unsigned int y) const;
-	unsigned int getWidth() const;
-	unsigned int getHeight() const;
+	RGB get(unsigned int x, unsigned int y) const
+    {
+        auto offset = ((y * this->width) + x) * 4; //Left to right, 4 bytes per pixel
+        auto r = this->image[offset];
+        auto g = this->image[offset+1];
+        auto b = this->image[offset+2];
+        auto a = this->image[offset+3];
+        return RGB((float)r/255.0f, (float)g / 255.0f, (float)b / 255.0f);
+    }
+
+    unsigned int getWidth() const
+    {
+        return width;
+    }
+
+	unsigned int getHeight() const
+    {
+        return height;
+    }
+
+    std::vector<unsigned char>& data()
+    {
+	    return image;
+	}
 };
+
+
