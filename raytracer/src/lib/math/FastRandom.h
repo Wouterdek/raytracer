3c16f0a78752f3f0f78c609de556151655d15830
Wouter <wouterdekrsmckr@gmail.com>
2019-10-24 00:40:59 +0200

    Fixed RNG issue
    mersenne twister wasn't seeded properly, also switched to pcg

--- raytracer/src/lib/math/FastRandom.h
+++ raytracer/src/lib/math/FastRandom.h
@@ -1,10 +1,25 @@
 #pragma once
 
 #include <random>
+#include "pcg_random.hpp"
+
+class RandDeviceSource
+{
+public:
+    RandDeviceSource()
+    {
+        // Make a random number engine, seeded with a real random value, if available
+        pcg_extras::seed_seq_from<std::random_device> seed_source;
+        randDev = pcg32_fast(seed_source);
+    }
+
+    //std::random_device randDev;
+    //std::mt19937 randDev;
+    pcg32_fast randDev;
+};
 
 namespace {
-    //thread_local std::random_device randDev();
-    thread_local std::mt19937 randDev;
+    thread_local RandDeviceSource randDevSrc;
 }
 
 class Rand
@@ -12,31 +27,31 @@ class Rand
 public:
     static float unit()
     {
-        return std::uniform_real_distribution<float>(0, 1)(randDev);
+        return std::uniform_real_distribution<float>(0, 1)(randDevSrc.randDev);
     }
 
     static float floatInRange(float a, float b)
     {
-        return std::uniform_real_distribution<float>(a, b)(randDev);
+        return std::uniform_real_distribution<float>(a, b)(randDevSrc.randDev);
     }
 
     static float floatInRange(float maxValue)
     {
-        return std::uniform_real_distribution<float>(0, maxValue)(randDev);
+        return std::uniform_real_distribution<float>(0, maxValue)(randDevSrc.randDev);
     }
 
     static int intInRange(int a, int b)
     {
-        return std::uniform_int_distribution<int>(a, b)(randDev);
+        return std::uniform_int_distribution<int>(a, b)(randDevSrc.randDev);
     }
 
     static int intInRange(int maxValue)
     {
-        return std::uniform_int_distribution<int>(0, maxValue)(randDev);
+        return std::uniform_int_distribution<int>(0, maxValue)(randDevSrc.randDev);
     }
 
     static float sampleStdNormalDist()
     {
-        return std::normal_distribution<float>(0, 1)(randDev);
+        return std::normal_distribution<float>(0, 1)(randDevSrc.randDev);
     }
 };
