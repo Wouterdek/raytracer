350df302435336f4c1a91e641dc7bea84ca34766
Wouter <wouterdekrsmckr@gmail.com>
2019-11-01 15:13:26 +0100

    Remove unused field in photon datatype

--- raytracer/src/lib/photonmapping/Photon.h
+++ raytracer/src/lib/photonmapping/Photon.h
@@ -5,16 +5,15 @@
 struct Photon
 {
     Point pos;
-    Vector3 incomingDir;
     Vector3 surfaceNormal;
     RGB energy;
     bool isCaustic;
 
     Photon(Point pos, Vector3 incomingDir, Vector3 surfaceNormal, RGB energy, bool isCaustic)
-            : pos(std::move(pos)), incomingDir(std::move(incomingDir)), surfaceNormal(std::move(surfaceNormal)), energy(energy), isCaustic(isCaustic)
+            : pos(std::move(pos)), surfaceNormal(std::move(surfaceNormal)), energy(std::move(energy)), isCaustic(isCaustic)
                {}
 
-    Photon() : pos(), incomingDir(), surfaceNormal(), energy(), isCaustic(false)
+    Photon() : pos(), surfaceNormal(), energy(), isCaustic(false)
         {}
 
     const Point& getPosition() const
