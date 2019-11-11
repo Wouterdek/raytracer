e17fa0eee776a162529cb584bed8097921f05f0f
Wouter <wouterdekrsmckr@gmail.com>
2019-11-11 19:40:55 +0100

    Implemented ImageMapEnvironment texture sampling

--- raytracer/src/lib/material/environment/ImageMapEnvironment.cpp
+++ raytracer/src/lib/material/environment/ImageMapEnvironment.cpp
@@ -13,5 +13,10 @@ ImageMapEnvironment* ImageMapEnvironment::cloneImpl() const
 
 RGB ImageMapEnvironment::getRadiance(const Scene &scene, const Vector3 &direction) const
 {
-    return RGB{};
+    auto latitude = std::asin(direction.y());
+    auto longtitude = M_PI + std::atan2(direction.z()/std::cos(latitude), direction.x()/std::cos(latitude));
+
+    auto x = texture->getWidth() * longtitude/(2.0f*M_PI);
+    auto y = texture->getHeight() * (1.0f - (((M_PI/2.0f) + latitude)/M_PI));
+    return this->texture->get(static_cast<int>(x), static_cast<int>(y));
 }
